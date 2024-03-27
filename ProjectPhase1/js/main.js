3c811fd4350e4043250234818ace2dcfd4616418
lu0u <155663212+lu0u@users.noreply.github.com>
2024-03-27 18:42:57 +0300

    productstop

--- ProjectPhase1/js/main.js
+++ ProjectPhase1/js/main.js
@@ -1,12 +1,60 @@
 
 
 //queries
+const topRating = document.querySelector("#topRating");
 
-//events
+const url = 'json/products.json';
+document.addEventListener('DOMContentLoaded', function () {
+  const topRating = document.querySelector("#topRating");
+  let products = [];
+  loadProducts();
 
-//loads
+  
+
+  async function loadProducts() {
+
+  try {
+    if (!localStorage.products) {
+      const response = await fetch(url);
+      products = await response.json();
+      localStorage.setItem("products", JSON.stringify(products));
+      console.log("Data fetched and stored in local storage:", products);
+    } else {
+      products = JSON.parse(localStorage.products);
+    }
+    // displayProducts(products);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+  // function displayProducts(products) {
+  //   const productsHTML = products.map(product => productToHTML(product)).join('');
+  //   topRating.innerHTML = productsHTML;
+  }
+  
+});
 
 
+
+function productToHTML(product) {
+  return `
+    <div class="product" id="">
+      <div class="product-info">
+        <img src="${product.image}" alt="" />
+        <h2 class="product-title">${product.title}</h2>
+        <p class="product-price">${product.price}</p>
+        <p class="product-description">${product.description}</p>
+        <p class="rating">${product.rating}</p>
+        <button type="button" class="purchase">Purchase</button>
+      </div>
+    </div>`;
+}
+
+// Call loadProducts to start fetching and displaying products
+
+
+
+
+  
 
 
 
@@ -36,3 +84,4 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
