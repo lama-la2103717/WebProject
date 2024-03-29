d72e23c7ea8c0488234779e364e2d6a7d354ed24
lama <126102146+lama-la2103717@users.noreply.github.com>
2024-03-29 17:24:19 +0300

    UseCase6(done) ,some edits in the css font

--- ProjectPhase1/js/sales.js
+++ ProjectPhase1/js/sales.js
@@ -36,12 +36,18 @@ mHeader.appendChild(headerBrandName);
 //queries
 const img =document.querySelector(".logoImg")
 const productContainer = document.querySelector('.product')
+const addB=document.querySelector('.addButton')
+const productForm=document.querySelector('.ProductForm')
+
+
 // const detailBtn =document.querySelector(".detailButton")
 
 
 
 //events
 // detailBtn.addEventListener('click',showDetails)
+addB.addEventListener('click', handelShowForm);
+
 
 //Lists
 let products = []
@@ -61,6 +67,18 @@ async function showProducts() {
 
     const filteredProducts = products.filter(product => product.brand.match(brandName));
     console.log(filteredProducts);
+
+    //search for a specific product by title 
+    const searchB = document.querySelector('#search');
+    searchB.addEventListener('input', () => {
+    const searched = filteredProducts.filter(p => {
+        const title = p.title.toLowerCase().includes(searchB.value.toLowerCase());
+        return title;
+    });
+
+    productContainer.innerHTML = displayProducts(searched);
+});
+    
 
     const productList= displayProducts(filteredProducts)
     productContainer.innerHTML =productList
@@ -87,16 +105,104 @@ function displayProducts(products) {
                 <p class="productPrice"><b>Price:</b> ${product.price}</p>
                 <p class="productStock"><b>Stock:</b> ${product.stock}</p>
                 <p class="productSold"><b>Sold:</b> ${product.stock}</p>
-                <button class="detailButton" data-title="${product.title}") onClick="showDetails(${product.id})">View Details</button>
+                <div class="productButton">
+                <button class="detailButton" data-title="${product.title}") onclick="showDetails(${product.id})">View Details</button>
+                <button class="updateButton" onclick="handelUpdateForm('${product.id}')">Update</button>
+                <button class="deleteButton"  onclick="deleteProduct('${product.id}')">Remove</button>
+                </div>
             </div>
-        `).join(" ")
+        `).join(" ");
 
-    return prodList
-
+    return prodList;
+    
+    
 }
 function showDetails(id){
     // console.log(id.value);
     const prod = products.find(p=>p.id == id)
-    console.log(prod);
+    alert(prod);
+    // console.log(prod);
     
 }
+
+function handelShowForm() {
+    addB.classList.add('hidden');
+    productForm.classList.remove('hidden');
+    const cancelB = document.querySelector('#cancel');
+    cancelB.addEventListener('click', handelHideForm);
+    productForm.addEventListener('submit', addProduct);
+}
+
+function handelHideForm(e) {
+    e.preventDefault();
+    productForm.reset();
+    productForm.classList.add('hidden');
+    addB.classList.remove('hidden');
+}
+
+//delete a product 
+function deleteProduct(prodId) {
+    const index = products.findIndex(p => p.id== prodId);
+    if (index !== -1) {
+        products.splice(index, 1);
+        localStorage.setItem('products', JSON.stringify(products));
+        displayProducts(products)
+    }
+}
+//add new product
+function addProduct(e) {
+    e.preventDefault();
+    const productId = Date.now() + Math.floor(Math.random() * 1000);
+    // console.log("callllled");
+    const newProduct = {
+        id: productId,
+        title: productForm.querySelector('#title').value,
+       image:productForm.querySelector('#thumbnailUrl').value,
+       price:productForm.querySelector('#price').value,
+       rating:productForm.querySelector('#rating').value,
+       description:productForm.querySelector('#description').value,
+       brand:brandName,
+       stock:productForm.querySelector('#stock').value,
+       sold:productForm.querySelector('#stock').value
+    };
+    let products = JSON.parse(localStorage.getItem('products'));
+    products.unshift(newProduct); // to add it to the Beginning
+    localStorage.setItem('products', JSON.stringify(products));
+    handelHideForm(e);
+    displayProducts(products)
+}
+//update product
+function handelUpdateForm(prodId) {
+    index = products.findIndex(p => p.id == prodId);
+    product = products[index];
+    document.querySelector('#id').value = product.id;
+    document.querySelector('#title').value = product.title;
+    document.querySelector('#thumbnailUrl').value = product.image;
+    document.querySelector('#price').value = product.price;
+    document.querySelector('#rating').value = product.rating;
+    document.querySelector('#description').value = product.description;
+    document.querySelector('#stock').value = product.stock;
+    handelShowForm(); 
+    productForm.removeEventListener('submit', addProduct);
+    productForm.addEventListener('submit', function(e) {
+        updateProduct(e, index); 
+    });
+}
+function updateProduct(e, index) {
+    e.preventDefault();
+    const updatedProduct = {
+        title: productForm.querySelector('#title').value,
+        image: productForm.querySelector('#thumbnailUrl').value,
+        price: productForm.querySelector('#price').value,
+        rating: productForm.querySelector('#rating').value,
+        description: productForm.querySelector('#description').value,
+        brand: brandName,
+        stock: productForm.querySelector('#stock').value,
+    };
+    products[index] = updatedProduct;
+    localStorage.setItem('products', JSON.stringify(products));
+    handelHideForm(e);
+    displayProducts(products);
+}
+
+
