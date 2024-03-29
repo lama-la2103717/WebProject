2a955d9a476f0a48b22e91e3e56162d355c88472
Ekram-eo2105434 <143485964+Ekram-eo2105434@users.noreply.github.com>
2024-03-29 13:09:15 +0300

    Update sales.js

--- ProjectPhase1/js/sales.js
+++ ProjectPhase1/js/sales.js
@@ -16,44 +16,71 @@ document.addEventListener('DOMContentLoaded',()=>{
 }
 
 )
+const urlParams = new URLSearchParams(window.location.search);
+
+//brand name
+//getting it from url
+const brandName = urlParams.get('brand');
+
+//h2 for the brand name on the right
+const headerBrandName = document.createElement('h2');
+
+//changeing th etxt of brand
+headerBrandName.textContent = brandName;
+
+//adding it to the hdr
+const mHeader = document.querySelector(".header");
+mHeader.appendChild(headerBrandName);
+
 
 //queries
-const mHeader = document.querySelector(".header");
 const img =document.querySelector(".logoImg")
+const productContainer = document.querySelector('.product')
+// const detailBtn =document.querySelector(".detailButton")
 
 
 
 //events
+// detailBtn.addEventListener('click',showDetails)
+
+//Lists
+let products = []
+showProducts()
 
 
 
-const urlParams = new URLSearchParams(window.location.search);
-const brandName = urlParams.get('brand');
 
-const headerBrandName = document.createElement('h2');
-headerBrandName.textContent = brandName;
-mHeader.appendChild(headerBrandName);
-
-async function loadProducts() {
-
-
-const response = await fetch('/json/products.json');
-const products = await response.json();
-const filteredProducts = products.filter(product => product.brand.match(brandName));
-console.log(products[0].brand);
-
-console.log(brandName);
-displayProducts(filteredProducts);}
+async function showProducts() {
+    if(!localStorage.products){
+        const data = await fetch('/json/products.json')
+        products = await data.json()
+    }
+    else
+        products=JSON.parse(localStorage.products)
 
 
-loadProducts();
+    const filteredProducts = products.filter(product => product.brand.match(brandName));
+    console.log(filteredProducts);
+
+    const productList= displayProducts(filteredProducts)
+    productContainer.innerHTML =productList
+    localStorage.products = JSON.stringify(products)
+
+
+
+
+    // const response = await fetch('/json/products.json');
+    // const products = await response.json();
+    // const filteredProducts = products.filter(product => product.brand.match(brandName));
+    // console.log(products[0].brand);
+
+    // console.log(brandName);
+    // displayProducts(filteredProducts);
+}
+
+
 function displayProducts(products) {
-    const productsList = document.querySelector('.product');
-    productsList.innerHTML = '';
-    products.forEach(product => {
-        const productContainer = document.createElement('div');
-        productContainer.classList.add('product');
-        productContainer.innerHTML = `
+    const prodList = products.map(product => `
             <div class="productInfo">
                 <img src="${product.image}" alt="${product.title}" />
                 <h4 class="productTitle">${product.title}</h4>
@@ -62,9 +89,9 @@ function displayProducts(products) {
                 <p class="productSold"><b>Sold:</b> ${product.stock}</p>
                 <button class="detailButton" data-title="${product.title}") onClick="showDetails(${product.id})">View Details</button>
             </div>
-        `;
-        productsList.appendChild(productContainer);
-    });
+        `).join(" ")
+
+    return prodList
 
 }
 function showDetails(id){
