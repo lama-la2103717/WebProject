2d4cae5f988e34f31d9c35df3e8fc525e81eb608
lu0u <155663212+lu0u@users.noreply.github.com>
2024-05-10 14:49:39 +0300

    uptadatedrepo

--- projectphase2/app/repo/ecommRepo.js
+++ projectphase2/app/repo/ecommRepo.js
@@ -34,5 +34,94 @@ class EcommRepo{
 
         }
     }
+    async getUser(userId){
+        try{
+            return prisma.user.findUnique(
+                { where: { userId } }
+            )
+        } catch (error) {
+            return { error: error.message }
+
+        }
+    }
+
+    async getPurchases(userId) {
+        try {
+            return prisma.purchaseHistory.findMany(
+                { where: { userId } }
+            )
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+    async updateUser(userId, user) {
+        try {
+            return await prisma.user.update({
+                where: { userId },
+                data: user
+            });
+        } catch (error) {
+            return { error: error.message };
+        }
+    }
+    
+   
+    async addPurchase(userId, purchaseHistory) {
+        purchaseHistory.userId = userId;
+        purchaseHistory.price = parseFloat(purchaseHistory.price.toString());
+        purchaseHistory.quantity = parseFloat(purchaseHistory.quantity.toString());
+        
+        try {
+            const user = await this.getUser(userId); // Corrected to await
+    
+            // Calculate total purchase price
+            const totalPurchasePrice = purchaseHistory.price * purchaseHistory.quantity;
+    
+            // Check if user has sufficient balance
+            if (user.balance >= totalPurchasePrice) {
+                user.balance -= totalPurchasePrice; // Deduct total purchase price from user balance
+            } else {
+                return { error: "Insufficient Balance" };
+            }
+    
+            await this.updateUser(userId, user); // Corrected to await
+    
+            const createdPurchase = await prisma.purchaseHistory.create({ data: purchaseHistory });
+            return createdPurchase;
+        } catch (error) {
+            return { error: error.message };
+        }
+    }
+    async getProductByTitle(title){
+        try {
+            return prisma.product.findUnique({
+                where: { title: title }
+            })
+        } catch (error) {
+            return { error: error.message }
+        }
+
+    }
+    async getProductByUsername(name){
+        try {
+            return prisma.product.findUnique({
+                where: { brand: name }
+            })
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+
+    async addProduct(product) {
+        try {
+            return prisma.product.create({
+                data: product
+            })
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+
+    
 }
 export default new EcommRepo()
