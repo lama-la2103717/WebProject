3b4bc4bf9d9e5bf33e4e2f2b005b7b1812eb2a5b
lama <126102146+lama-la2103717@users.noreply.github.com>
2024-05-11 03:03:20 +0300

    page.jsx

--- projectphase2/app/customers/[username]/page.jsx
+++ projectphase2/app/customers/[username]/page.jsx
@@ -1,31 +1,46 @@
 import React from 'react';
 import styles from "@/app/page.module.css";
 import ecomRepo from '@/app/repo/ecomRepo';
-// import purchaseHistoryData from '@/app/data/purchasehistory.json';
-// import userData from '@/app/data/users.json';
-export default async function page({params}) {
-  console.log(params)
 
-  const purchase = await ecomRepo.getCustomerHistoryByName(params.username)
-  const money = purchase.userPurchases.map(p =>p.price)
-  const totalAmount= money.reduce((accumaltor , currentValue)=>accumaltor+currentValue,0);
+export default async function page({ params }) {
+  console.log(params);
+  const purchase = await ecomRepo.getCustomerHistoryByName(params.username);
+  const money = purchase.userPurchases.map(p => p.price);
+  const totalAmount = money.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+  const brandCounts = purchase.userPurchases.reduce((counts, purchase) => {
+    counts[purchase.brandName] = (counts[purchase.brandName] || 0) + 1;
+    return counts;
+  }, {});
+
+  let mostOrderedBrand = "";
+  let maxOrders = 0;
+  for (const brand in brandCounts) {
+    if (brandCounts[brand] > maxOrders) {
+      mostOrderedBrand = brand;
+      maxOrders = brandCounts[brand];
+    }
+  }
+
   return (
     <div>
-
-    <h1 className={styles.h1}>Customer Statistics</h1>
-    <table className={styles.table}>
-      <tbody>
-        <tr>
-          <td className={styles.td}>Total Orders</td>
-          <td className={styles.td}>{purchase.userPurchases.length}</td>
-        </tr>
-
-        <tr>
-          <td className={styles.td}>Total Amount Paid</td>
-          <td className={styles.td}>{totalAmount}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  )
+      <h1 className={styles.h1}>Customer Statistics</h1>
+      <table className={styles.table}>
+        <tbody>
+          <tr>
+            <td className={styles.td}>Total Orders</td>
+            <td className={styles.td}>{purchase.userPurchases.length}</td>
+          </tr>
+          <tr>
+            <td className={styles.td}>Total Amount Paid</td>
+            <td className={styles.td}>{totalAmount}</td>
+          </tr>
+          <tr>
+            <td className={styles.td}>Most Ordered Brand</td>
+            <td className={styles.td}>{mostOrderedBrand}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
