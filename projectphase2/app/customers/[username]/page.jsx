4594a3f9f653af69f9cfbaa5b7c85e63022f0508
lama <126102146+lama-la2103717@users.noreply.github.com>
2024-05-11 13:02:11 +0300

    updated css for stat

--- projectphase2/app/customers/[username]/page.jsx
+++ projectphase2/app/customers/[username]/page.jsx
@@ -24,7 +24,7 @@ export default async function page({ params }) {
   }
 
   return (
-    <div>
+    <div className={styles.div}>
       <Link href={'/html/main.html'} className={styles.back}>back</Link>
       <h1 className={styles.h1}>Customer Statistics</h1>
       <table className={styles.table}>
